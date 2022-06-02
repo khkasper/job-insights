84994d2a22a584908a8cf6660131391e4b0f1563
Kristiano <kristianokasper@gmail.com>
2022-06-01 23:53:57 -0300

    req 10 ok

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,12 @@
-# from src.counter import count_ocurrences
+import pytest
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("src/jobs.csv", "test") == 2687
+    assert count_ocurrences("src/jobs.csv", "salary") == 598
+    assert count_ocurrences("src/jobs.csv", "job") == 3454
+    assert count_ocurrences("src/jobs.csv", "python") == 1639
+    assert count_ocurrences("src/jobs.csv", "medical") == 1639
+    with pytest.raises(FileNotFoundError):
+        count_ocurrences("src/test.csv", "test")
