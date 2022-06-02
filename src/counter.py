7bbb03da6e38366ad4173241c3227945d6947dc0
Kristiano <kristianokasper@gmail.com>
2022-06-02 00:28:36 -0300

    req 11 ok

--- src/counter.py
+++ src/counter.py
@@ -3,11 +3,3 @@ def count_ocurrences(path, word):
     read_data = file.read()
     word_count = read_data.lower().count(word.lower())
     return word_count
-
-
-if __name__ == "__main__":
-    print(count_ocurrences("src/jobs.csv", "test"))
-    print(count_ocurrences("src/jobs.csv", "salary"))
-    print(count_ocurrences("src/jobs.csv", "job"))
-    print(count_ocurrences("src/jobs.csv", "python"))
-    print(count_ocurrences("src/jobs.csv", "medical"))
